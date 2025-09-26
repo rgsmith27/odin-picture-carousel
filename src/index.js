884320db21581557e5571e57672c7563f2fc7dd4
Rowin <rgsmith@colgate.edu>
2025-09-26 11:14:32 -0400

    implemented slideShiftRight and slideShiftLeft implemented basic logic for slides
    todo:
    add images to slides
    test left and right shift
    add left and right shift buttons
    add position indicator

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
 import "./styles.css";
+
+const tape = document.querySelector(".slide-tape");
+const slides = Array.from(document.querySelectorAll(".slide"));
+const slideWidth = slides[0].getBoundingClientRect().width;
+const slideCount = slides.length();
+const startPosition = parseInt(tape.style.left);
+
+let slideIndex = 0;
+
+const slideShiftRight = () => {
+  if (slideIndex == slideCount - 1) {
+    slideIndex = 0;
+  } else {
+    slideIndex++;
+  }
+  const newPosition = startPosition - slideIndex * slideWidth;
+  tape.styles.left = newPosition + "px";
+};
+
+const slideShiftLeft = () => {
+  if (slideIndex == 0) {
+    slideIndex = slideCount - 1;
+  } else {
+    slideIndex--;
+  }
+  const newPosition = startPosition - slideIndex * slideWidth;
+  tape.styles.left = newPosition + "px";
+};
